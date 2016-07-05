5f222643f43bd171f578cd02cb24df82e7b0083c
cklynch2 <cklynch2@gmail.com>
2016-07-05 15:23:21 -0400

    Can't go around tryna change those puppy names yo

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,57 +14,47 @@
 /*: question1
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
-// write your code here
+// Use var because your bank account balance will change over time. When you option click you can see that this line of code implicitly makes accountBalance type Int because I assigned it to an Int value.
 
-
-
-
+var accountBalance = 1234
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
-// write your code here
+// Use let because you want the name to be a constant. Again, Swift type inference means that the compiler figures out that puppyName is of type String because I assigned it to a string constant.
 
-
-
-
+let puppyName = "Bella"
 /*: question3
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
-// write your code here
+// You could also just print the string literal "Bella" since it is a constant. The only real benefit of putting it into a constant puppyName is so that we can access/utilize the value in multiple places.
 
-
-
-
+print(puppyName)
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
 // write your code here
 
-
-
-
+print("I just got a new puppy named \(puppyName) and she is awesome!")
 /*: question5
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
-// write your code here
+// Same syntax for interpolating an int or a string!!
 
-
+print("I have $\(accountBalance) in my bank account.")
 
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
 // write your code here
-
-
-
-
+accountBalance += 100
+print("I now have $\(accountBalance).")
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
-// write your code here
+// You cannot do this because you declared puppyName as a constant using 'let'. Yo, you're a bad puppy parent if you go around changing her name all the time! If you try to change the name, Xcode raises the error and suggests that you change 'let' to 'var' in order to mutate puppyName.
 
-
+// puppyName = "Bug"
 
 
 //: [Solution](solution)
